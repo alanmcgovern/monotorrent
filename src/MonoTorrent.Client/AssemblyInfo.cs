8bb4a31e6967eb48be5aa5617c1c599522a209d9
Alan McGovern <alan.mcgovern@gmail.com>
2006-12-23 00:06:07 +0000

    Added some AssemblyInfo
    svn path=/trunk/bitsharp/; revision=69977

---
+++ src/MonoTorrent.Client/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+
+[assembly: AssemblyCopyright("Alan McGovern")]
+[assembly: AssemblyDescription("MonoTorrent Utilities")]
+[assembly: AssemblyFileVersion("0.1")]
+[assembly: AssemblyInformationalVersion("0.1")]
+[assembly: AssemblyProduct("MonoTorrent Class Library")]
+[assembly: AssemblyTitle("MonoTorrent Class Library")]
+[assembly: AssemblyVersion("0.1")]
