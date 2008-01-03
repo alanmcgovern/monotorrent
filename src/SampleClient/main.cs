0dc07421bd2d175d5f8e26de21fef7cafbe5112c
Alan McGovern <alan.mcgovern@gmail.com>
2008-01-03 23:38:52 +0000

    Default to unlimited download speed
    svn path=/trunk/bitsharp/; revision=92218

--- src/SampleClient/main.cs
+++ src/SampleClient/main.cs
@@ -73,7 +73,7 @@ namespace MonoTorrent
             // 50 open connections - should never really need to be changed
             // Unlimited download speed - valid range from 0 -> int.Max
             // Unlimited upload speed - valid range from 0 -> int.Max
-            TorrentSettings torrentDefaults = new TorrentSettings(4, 150, 100, 25);
+            TorrentSettings torrentDefaults = new TorrentSettings(4, 150, 0, 25);
 
             // Create an instance of the engine.
             engine = new ClientEngine(engineSettings);
