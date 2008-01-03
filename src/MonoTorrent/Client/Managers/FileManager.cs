ffa14f0098ce0bc0de2134ef53e45b9259cbd49c
Alan McGovern <alan.mcgovern@gmail.com>
2008-01-03 16:11:43 +0000

    Removed a #warning
    svn path=/trunk/bitsharp/; revision=92185

--- src/MonoTorrent/Client/Managers/FileManager.cs
+++ src/MonoTorrent/Client/Managers/FileManager.cs
@@ -244,7 +244,7 @@ namespace MonoTorrent.Client
         {
             try
             {
-#warning Store all the fast resume in a 'data' file in a known location instead?
+                // FIXME: #warning Store all the fast resume in a 'data' file in a known location instead?
                 // If we don't know where the .torrent is on disk, then don't save
                 // fast resume data.
                 if (string.IsNullOrEmpty(manager.Torrent.TorrentPath))
