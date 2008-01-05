93493c3535b2115ac5945915ac549ad2543dc3db
Alan McGovern <alan.mcgovern@gmail.com>
2008-01-05 16:13:52 +0000

    Added assembly info to the solution
    svn path=/trunk/bitsharp/; revision=92300

---
+++ src/MonoTorrent/Common/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+
+
+[assembly: AssemblyTitle("MonoTorrent")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MonoTorrent")]
+[assembly: AssemblyCopyright("Copyright © Alan McGovern")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: CLSCompliant(true)]
