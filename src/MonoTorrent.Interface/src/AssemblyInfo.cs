55041a0f5fe9cf4c8047cfeb6b1e8a84457b0b6d
Alan McGovern <alan.mcgovern@gmail.com>
2008-01-30 14:39:20 +0000

    Goodbye old crappy interface!
    svn path=/trunk/bitsharp/; revision=94379

--- src/MonoTorrent.Interface/src/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyVersion("0.1")]
-[assembly: AssemblyInformationalVersion("0.1")]
-[assembly: AssemblyTitle("MonoTorrent")]
-[assembly: AssemblyDescription("MonoTorrent Utilities")]
-[assembly: AssemblyCopyright("piotr")]
