5a9212b33357ddddb592349b121155093e7ae1b1
Alan McGovern <alan.mcgovern@gmail.com>
2008-06-18 17:59:29 +0000

    Updating the makefiles
    svn path=/trunk/bitsharp/; revision=106136

--- src/MonoTorrent/AssemblyInfo.cs
+++ src/MonoTorrent/AssemblyInfo.cs
@@ -3,13 +3,12 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.IO;
 
-[assembly: InternalsVisibleTo("SampleClient")]
 [assembly: InternalsVisibleTo("MonoTorrent.Tests")]
 [assembly: AssemblyCopyright("Alan McGovern, Gregor Buguer;  MIT X11 Licensed")]
 [assembly: AssemblyDescription("MonoTorrent Utilities")]
-[assembly: AssemblyFileVersion("0.40")]
-[assembly: AssemblyInformationalVersion("0.40")]
+[assembly: AssemblyFileVersion("0.3")]
+[assembly: AssemblyInformationalVersion("0.3")]
 [assembly: AssemblyProduct("MonoTorrent Common Library")]
 [assembly: AssemblyTitle("MonoTorrent Common Library")]
-[assembly: AssemblyVersion("0.40")]
+[assembly: AssemblyVersion("0.3")]
 //[assembly: CLSCompliant (true)] 
