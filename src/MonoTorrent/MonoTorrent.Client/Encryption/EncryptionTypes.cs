fe56b6346963b3a8fbf3bf3d5e16a1e6023783b9
Alan McGovern <alan.mcgovern@gmail.com>
2008-06-19 17:19:25 +0000

    Added a 'none' to represent no connection methods
    svn path=/trunk/bitsharp/; revision=106237

--- src/MonoTorrent/MonoTorrent.Client/Encryption/EncryptionTypes.cs
+++ src/MonoTorrent/MonoTorrent.Client/Encryption/EncryptionTypes.cs
@@ -7,6 +7,7 @@ namespace MonoTorrent.Client.Encryption
     [Flags]
     public enum EncryptionTypes
     {
+        None        = 0,
         PlainText   = 1 << 0,
         RC4Header   = 1 << 1,
         RC4Full     = 1 << 2,
