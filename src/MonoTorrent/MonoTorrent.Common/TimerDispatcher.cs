da8ca4661f0d735bc4d6f14d55c18a84c24a2617
Alan McGovern <alan.mcgovern@gmail.com>
2008-07-21 15:52:06 +0000

    Add the SSDP TimerDispatcher class
    svn path=/trunk/bitsharp/; revision=108364

---
+++ src/MonoTorrent/MonoTorrent.Common/TimerDispatcher.cs
@@ -0,0 +1,148 @@
+//
+// TimeoutDispatcher.cs
+//
+// Author:
+//   Aaron Bockover <abockover@novell.com>
+//
+// Copyright (C) 2008 Novell, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Mono.Ssdp.Internal
+{
+    public delegate void TimeoutHandler ();
+
+    public class TimeoutDispatcher
+    {
+        private static uint timeout_ids = 1;
+        
+        private struct TimeoutItem : IComparable<TimeoutItem>
+        {
+            public uint Id;
+            public DateTime Trigger;
+            public TimeoutHandler Handler;
+            
+            public int CompareTo (TimeoutItem item)
+            {
+                return Trigger.CompareTo (item.Trigger);
+            }
+            
+            public override string ToString ()
+            {
+                return String.Format ("{0} ({1})", Id, Trigger);
+            }
+        }
+        
+        private readonly object wait_mutex = new object ();
+        private AutoResetEvent wait;
+
+        private List<TimeoutItem> timeouts = new List<TimeoutItem> ();
+        
+        public uint Enqueue (DateTime expire, TimeoutHandler handler)
+        {
+            lock (this) {
+                TimeoutItem item = new TimeoutItem ();
+                item.Id = timeout_ids++;
+                item.Trigger = expire;
+                item.Handler = handler;
+                
+                Enqueue (ref item);
+                
+                if (timeouts.Count == 1) {
+                    Start ();
+                }
+                
+                return item.Id;
+            }
+        }
+        
+        private void Enqueue (ref TimeoutItem item)
+        {
+            lock (timeouts) {
+                int index = timeouts.BinarySearch (item);
+                timeouts.Insert (index >= 0 ? index : ~index, item);
+                
+                if (index == 0 && timeouts.Count > 1) {
+                    lock (wait_mutex) {
+                        if (wait != null) {
+                            wait.Set ();
+                        }
+                    }
+                }
+            }
+        }
+        
+        private void Dequeue (uint id)
+        {
+            lock (timeouts) {
+                // FIXME: Comparer for BinarySearch
+                for (int i = 0; i < timeouts.Count; i++) {
+                    if (timeouts[i].Id == id) {
+                        timeouts.RemoveAt (i);
+                        return;
+                    }
+                }
+            }
+        }
+        
+        private void Start ()
+        {
+            wait = new AutoResetEvent (false);
+            ThreadPool.QueueUserWorkItem (TimerThread);
+        }
+        
+        private void TimerThread (object state)
+        {
+            while (timeouts.Count > 0) {
+                TimeoutItem item;
+                lock (timeouts) {
+                    item = timeouts[0];
+                }
+                
+                bool restart = false;
+                TimeSpan interval = item.Trigger - DateTime.Now;
+                if (interval >= TimeSpan.Zero) {
+                    if (!wait.WaitOne (interval, false)) {
+                        item.Handler ();
+                    } else {
+                        restart = true;
+                    }
+                }
+                
+                if (restart) {
+                    continue;
+                }
+                
+                Dequeue (item.Id);
+            }
+            
+            lock (wait_mutex) {
+                if (wait != null) {
+                    wait.Set ();
+                }
+            }
+        }
+    }
+}
