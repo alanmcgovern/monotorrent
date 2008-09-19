d904ef1eaf981bb2a64413b4b5e311eece97e2cf
Alan McGovern <alan.mcgovern@gmail.com>
2008-09-19 17:50:45 +0000

    InitialiseTask follows the specification properly. Only send FindNode requests to nodes which are closer to the local node than previously queried nodes. When all pending findnodes are completed, complete the task.
    svn path=/trunk/bitsharp/; revision=113561

--- src/MonoTorrent.Dht/MonoTorrent.Dht/Tasks/InitialiseTask.cs
+++ src/MonoTorrent.Dht/MonoTorrent.Dht/Tasks/InitialiseTask.cs
@@ -1,13 +1,14 @@
 using MonoTorrent.Dht.Messages;
 using System;
 using System.Net;
+using System.Collections.Generic;
 
 namespace MonoTorrent.Dht.Tasks
 {
     internal class InitialiseTask : Task
     {
-        private const int MaxNodes = 50;
-        int nodesFound = 0;
+        int activeRequests = 0;
+        SortedList<NodeId, NodeId> nodes = new SortedList<NodeId, NodeId>();
         DhtEngine engine;
             
         public InitialiseTask(DhtEngine engine)
@@ -20,44 +21,35 @@ namespace MonoTorrent.Dht.Tasks
             if (Active)
                 return;
 
-            DhtEngine.MainLoop.QueueTimeout(TimeSpan.FromMinutes(1), delegate {
-                if (Active)
-                    RaiseComplete(new TaskCompleteEventArgs(this));
-                return false;
-            });
+            // This shouldn't be needed
+            //DhtEngine.MainLoop.QueueTimeout(TimeSpan.FromMinutes(1), delegate {
+            //    if (Active)
+            //        RaiseComplete(new TaskCompleteEventArgs(this));
+            //    return false;
+            //});
 
             Active = true;
-            engine.RoutingTable.NodeAdded += NodeAdded;
-            Node utorrent = new Node(NodeId.Create(), new System.Net.IPEndPoint(Dns.GetHostEntry("router.bittorrent.com").AddressList[0], 6881));
-            Node node2 = new Node(NodeId.Create(), new IPEndPoint(Dns.GetHostEntry("router.utorrent.com").AddressList[0], 6881));
-            engine.Add(utorrent);
-            engine.Add(node2);
-            //issue here id this 2 node never answer we will never raise complete!
-            //but it seems logical because we will never init until we get a node to bootstrap!
-        }
-        
-        void TaskComplete(object sender, TaskCompleteEventArgs e)
-        {
-            //we need to have at least one node who answer new nodes...
-            SendQueryEventArgs args = (SendQueryEventArgs)e;
-            args.Task.Completed -= TaskComplete;
-            if (!args.TimedOut)
-                nodesFound++;
 
-            //node added is call before this because handle is done before message sent
-            //so if we are back to 0, this mean that we have no node that we wait answer 
-            if (nodesFound >= MaxNodes)
+            Node utorrent = new Node(NodeId.Create(), new System.Net.IPEndPoint(Dns.GetHostEntry("router.bittorrent.com").AddressList[0], 6881));
+            SendFindNode(utorrent);
+        }
+
+        private void FindNodeComplete(object sender, TaskCompleteEventArgs e)
+        {
+            e.Task.Completed -= FindNodeComplete;
+            activeRequests--;
+
+            SendQueryEventArgs args = (SendQueryEventArgs)e;
+            if (!args.TimedOut)
+            {
+                FindNodeResponse response = (FindNodeResponse)args.Response;
+                foreach (Node n in Node.FromCompactNode(response.Nodes))
+                    SendFindNode(n);
+            }
+
+            if (activeRequests == 0)
                 RaiseComplete(new TaskCompleteEventArgs(this));
         }
-        
-        private void NodeAdded(object o, NodeAddedEventArgs e)
-        {
-            // If we reached our target amount of nodes or we've run out of time, complete the task
-            // Otherwise keep firing off FindNode requests to find nodes close to our own.
-             SendQueryTask task = new SendQueryTask(engine, new FindNode(engine.LocalId, engine.LocalId), e.Node);
-             task.Completed += TaskComplete;
-             task.Execute();            
-          }
 
         protected override void RaiseComplete(TaskCompleteEventArgs e)
         {
@@ -65,8 +57,30 @@ namespace MonoTorrent.Dht.Tasks
                 return;
 
             Active = false;
-            engine.RoutingTable.NodeAdded -= NodeAdded;
             base.RaiseComplete(e);
+        }
+
+        private void SendFindNode(Node node)
+        {
+            NodeId distance = node.Id.Xor(engine.LocalId);
+            if (nodes.Count < Bucket.MaxCapacity)
+            {
+                nodes.Add(distance, node.Id);
+            }
+            else if (distance < nodes.Keys[nodes.Count - 1] && !nodes.Values.Contains(node.Id))
+            {
+                nodes.RemoveAt(nodes.Count - 1);
+                nodes.Add(distance, node.Id);
+            }
+            else
+            {
+                return;
+            }
+
+            activeRequests++;
+            FindNode request = new FindNode(engine.LocalId, engine.LocalId);
+            SendQueryTask task = new SendQueryTask(engine, request, node);
+            task.Completed += FindNodeComplete;
         }
     }
 }
