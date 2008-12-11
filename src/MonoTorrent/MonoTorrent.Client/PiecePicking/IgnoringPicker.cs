dc5ff18d9e17bc8863e3415af99d284ffc3e0ec6
Alan McGovern <alan.mcgovern@gmail.com>
2008-12-11 23:55:16 +0000

    Add IgnoringPicker - this uses an existing bitfield to zero out values we do not want to pick
    svn path=/trunk/bitsharp/; revision=121373

---
+++ src/MonoTorrent/MonoTorrent.Client/PiecePicking/IgnoringPicker.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MonoTorrent.Common;
+using MonoTorrent.Client.Messages;
+
+namespace MonoTorrent.Client.PiecePicking
+{
+    public class IgnoringPicker : PiecePicker
+    {
+        BitField bitfield;
+        BitField temp;
+
+        public IgnoringPicker(BitField bitfield, PiecePicker picker)
+            : base(picker)
+        {
+            this.bitfield = bitfield;
+            this.temp = new BitField(bitfield.Length);
+        }
+
+        public override MessageBundle PickPiece(PeerId id, BitField peerBitfield, List<PeerId> otherPeers, int startIndex, int endIndex, int count)
+        {
+            // Invert 'bitfield' and AND it with the peers bitfield
+            // Any pieces which are 'true' in the bitfield will not be downloaded
+            temp.SetAll(false).Or(peerBitfield).NAnd(bitfield);
+            return base.PickPiece(id, temp, otherPeers, startIndex, endIndex, count);
+        }
+    }
+}
