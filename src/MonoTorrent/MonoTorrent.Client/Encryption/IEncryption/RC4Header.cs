6afb295adc7d279b7a682db46b2262cc11fa999b
Alan McGovern <alan.mcgovern@gmail.com>
2008-12-21 19:13:53 +0000

    RC4Header shouldn't inherit from PlainTextEncryption
    svn path=/trunk/bitsharp/; revision=121964

--- src/MonoTorrent/MonoTorrent.Client/Encryption/IEncryption/RC4Header.cs
+++ src/MonoTorrent/MonoTorrent.Client/Encryption/IEncryption/RC4Header.cs
@@ -32,8 +32,34 @@ using System.Text;
 
 namespace MonoTorrent.Client.Encryption
 {
-    public class RC4Header : PlainTextEncryption
+    public class RC4Header : IEncryption
     {
-        
+        public RC4Header()
+        {
+        }
+
+        public void Decrypt(byte[] buffer)
+        {
+        }
+
+        public void Decrypt(byte[] buffer, int offset, int count)
+        {
+        }
+
+        public void Decrypt(byte[] src, int srcOffset, byte[] dest, int destOffset, int count)
+        {
+        }
+
+        public void Encrypt(byte[] buffer)
+        {
+        }
+
+        public void Encrypt(byte[] buffer, int offset, int count)
+        {
+        }
+
+        public void Encrypt(byte[] src, int srcOffset, byte[] dest, int destOffset, int count)
+        {
+        }
     }
 }
