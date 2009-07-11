73bd28b5299f27f826a809b590e333e7b8e9fd57
Alan McGovern <alan.mcgovern@gmail.com>
2009-07-11 21:56:21 +0000

    Correctly handle the case when a speed monitor has less datapoints than the averaging period.
    svn path=/trunk/bitsharp/; revision=137750

--- src/MonoTorrent/MonoTorrent.Common/SpeedMonitor.cs
+++ src/MonoTorrent/MonoTorrent.Common/SpeedMonitor.cs
@@ -8,10 +8,10 @@ namespace MonoTorrent.Common
     {
         private const int DefaultAveragePeriod = 12;
 
+        private int averagingPeriod;
         private long total;
         private int speed;
-        private int speedIndex;
-        private int[] speeds;
+        private List<int> speeds;
         private int lastUpdated;
         private long tempRecvCount;
 
@@ -35,8 +35,9 @@ namespace MonoTorrent.Common
 
         public SpeedMonitor(int averagingPeriod)
         {
+            this.averagingPeriod = averagingPeriod;
             this.lastUpdated = Environment.TickCount;
-            this.speeds = new int[averagingPeriod];
+            this.speeds = new List<int>(averagingPeriod);
         }
 
 
@@ -57,11 +58,9 @@ namespace MonoTorrent.Common
         {
             this.total = 0;
             this.speed = 0;
-            this.speedIndex = 0;
             this.tempRecvCount = 0;
             this.lastUpdated = Environment.TickCount;
-            for (int i = 0; i < this.speeds.Length; i++)
-                speeds[i] = 0;
+            this.speeds.Clear();
         }
 
 
@@ -78,28 +77,23 @@ namespace MonoTorrent.Common
             // Take the amount of bytes sent since the last tick and divide it by the number of seconds
             // since the last tick. This gives the calculated bytes/second transfer rate.
             // difference is in miliseconds, so divide by 1000 to get it in seconds
-            this.speeds[this.speedIndex++] = (int)(tempRecvCount / (difference / 1000.0));
-
-            // If we've gone over the array bounds, reset to the first index
-            // to start overwriting the old values
-            if (this.speedIndex == speeds.Length)
-                this.speedIndex = 0;
+            if (speeds.Count == this.averagingPeriod)
+                speeds.RemoveAt (0);
+            speeds.Add ((int)(tempRecvCount / (difference / 1000.0)));
 
             // What we do here is add up all the bytes/second readings held in each array
             // and divide that by the number of non-zero entries. The number of non-zero entries
             // is given by ArraySize - count. This is to avoid the problem where a connection which
             // is just starting would have a lot of zero entries making the speed estimate inaccurate.
-            for (int i = 0; i < this.speeds.Length; i++)
+            for (int i = 0; i < this.speeds.Count; i++)
             {
-                if (this.speeds[i] == 0)
+                if (speeds.Count != averagingPeriod)
                     count++;
 
                 total += this.speeds[i];
             }
-            if (count == speeds.Length)
-                count--;
 
-            this.speed = (int)(total / (speeds.Length - count));
+            this.speed = (int)(total / Math.Max (1, speeds.Count - count));
             this.tempRecvCount = 0;
             this.lastUpdated = Environment.TickCount;
         }
