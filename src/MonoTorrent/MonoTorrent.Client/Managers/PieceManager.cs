5343b596494d22ba0d3203f322d7a138926b409d
Alan McGovern <alan.mcgovern@gmail.com>
2009-10-11 22:56:08 +0000

    Backport from trunk: To prevent possible deadlock, PieceManager should use the async 'BeginGetHash' method instead of the sync 'GetHash' method.
    svn path=/branches/bitsharp-0.80/; revision=143919

--- src/MonoTorrent/MonoTorrent.Client/Managers/PieceManager.cs
+++ src/MonoTorrent/MonoTorrent.Client/Managers/PieceManager.cs
@@ -106,27 +106,29 @@ namespace MonoTorrent.Client
 						return;
 
 					// Hashcheck the piece as we now have all the blocks.
-					byte[] hash = id.Engine.DiskManager.GetHash(id.TorrentManager, piece.Index);
-					bool result = id.TorrentManager.Torrent.Pieces.IsValid(hash, piece.Index);
-					id.TorrentManager.Bitfield[data.PieceIndex] = result;
+                    id.Engine.DiskManager.BeginGetHash (id.TorrentManager, piece.Index, delegate (object o) {
+					    byte[] hash = (byte[]) o;
+					    bool result = id.TorrentManager.Torrent.Pieces.IsValid(hash, piece.Index);
+					    id.TorrentManager.Bitfield[data.PieceIndex] = result;
 
-					ClientEngine.MainLoop.Queue(delegate
-					{
-						id.TorrentManager.PieceManager.UnhashedPieces[piece.Index] = false;
+					    ClientEngine.MainLoop.Queue(delegate
+					    {
+						    id.TorrentManager.PieceManager.UnhashedPieces[piece.Index] = false;
 
-						id.TorrentManager.HashedPiece(new PieceHashedEventArgs(id.TorrentManager, piece.Index, result));
-						List<PeerId> peers = new List<PeerId>(piece.Blocks.Length);
-						for (int i = 0; i < piece.Blocks.Length; i++)
-							if (piece.Blocks[i].RequestedOff != null && !peers.Contains(piece.Blocks[i].RequestedOff))
-								peers.Add(piece.Blocks[i].RequestedOff);
+						    id.TorrentManager.HashedPiece(new PieceHashedEventArgs(id.TorrentManager, piece.Index, result));
+						    List<PeerId> peers = new List<PeerId>(piece.Blocks.Length);
+						    for (int i = 0; i < piece.Blocks.Length; i++)
+							    if (piece.Blocks[i].RequestedOff != null && !peers.Contains(piece.Blocks[i].RequestedOff))
+								    peers.Add(piece.Blocks[i].RequestedOff);
 
-						for (int i = 0; i < peers.Count; i++)
-							if (peers[i].Connection != null)
-								id.Peer.HashedPiece(result);
+						    for (int i = 0; i < peers.Count; i++)
+							    if (peers[i].Connection != null)
+								    id.Peer.HashedPiece(result);
 
-						// If the piece was successfully hashed, enqueue a new "have" message to be sent out
-						if (result)
-							id.TorrentManager.finishedPieces.Enqueue(piece.Index);
+						    // If the piece was successfully hashed, enqueue a new "have" message to be sent out
+						    if (result)
+							    id.TorrentManager.finishedPieces.Enqueue(piece.Index);
+					    });
 					});
 				});
                 
