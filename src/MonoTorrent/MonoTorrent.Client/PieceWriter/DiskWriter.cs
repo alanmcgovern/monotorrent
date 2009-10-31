cf7686ef1da3e9794ca363252e0265d49c8bbeb7
Alan McGovern <alan.mcgovern@gmail.com>
2009-10-31 01:42:50 +0000

    If the TorrentManager is available, use it to get the total size of the torrent rather than summing it up. Speeds up hashing on torrents with hundreds of files.
    svn path=/trunk/bitsharp/; revision=145128

--- src/MonoTorrent/MonoTorrent.Client/PieceWriter/DiskWriter.cs
+++ src/MonoTorrent/MonoTorrent.Client/PieceWriter/DiskWriter.cs
@@ -59,7 +59,12 @@ namespace MonoTorrent.Client.PieceWriters
             long offset = data.Offset;
             int count = data.Count;
             IList<TorrentFile> files = data.Files;
-            long fileSize = Toolbox.Accumulate<TorrentFile>(files, delegate(TorrentFile f) { return f.Length; });
+            long fileSize;
+            //if (data.Manager != null)
+            //    fileSize = data.Manager.Torrent.Size;
+            //else
+                fileSize = Toolbox.Accumulate<TorrentFile>(files, delegate(TorrentFile f) { return f.Length; });
+
             if (offset < 0 || offset + count > fileSize)
                 throw new ArgumentOutOfRangeException("offset");
 
