f2ad7751c973dd6dca5b66afeef76e5820bbb2bc
Alan McGovern <alan.mcgovern@gmail.com>
2009-11-19 23:10:30 +0000

    If the initial byte[] is of length zero, replace it with a length 1 array to prevent crashers later on.
    svn path=/trunk/bitsharp/; revision=146582

--- src/MonoTorrent/MonoTorrent.Client/Encryption/BigInteger/BigInteger.cs
+++ src/MonoTorrent/MonoTorrent.Client/Encryption/BigInteger/BigInteger.cs
@@ -121,6 +121,8 @@ namespace Mono.Math
 
         public BigInteger(byte[] inData)
         {
+            if (inData.Length == 0)
+                inData = new byte[1];
             length = (uint)inData.Length >> 2;
             int leftOver = inData.Length & 0x3;
 
