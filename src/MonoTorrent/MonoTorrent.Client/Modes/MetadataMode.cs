6a8c6c57626de7402c9ba405a719818117739672
Alan McGovern <alan.mcgovern@gmail.com>
2010-01-16 15:54:05 +0000

    Use .Equals as we're comparing an InfoHash and byte []
    svn path=/trunk/bitsharp/; revision=149697

--- src/MonoTorrent/MonoTorrent.Client/Modes/MetadataMode.cs
+++ src/MonoTorrent/MonoTorrent.Client/Modes/MetadataMode.cs
@@ -234,7 +234,7 @@ namespace MonoTorrent.Client
             byte[] calculatedInfoHash;
             using (SHA1 sha = HashAlgoFactory.Create<SHA1>())
                 calculatedInfoHash = sha.ComputeHash(stream.ToArray());
-            if (Manager.InfoHash != calculatedInfoHash)
+            if (!Manager.InfoHash.Equals (calculatedInfoHash))
                 throw new Exception("invalid metadata");//restart ?
 
             BEncodedValue d = BEncodedValue.Decode(stream);
