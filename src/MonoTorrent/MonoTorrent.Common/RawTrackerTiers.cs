561a7dfab4af747d826d3e606110beb34448c6fa
Aaron Simmonds <aaronsace@hotmail.com>
2018-06-22 23:03:56 +0100

    Update RawTrackerTiers.cs

--- src/MonoTorrent/MonoTorrent.Common/RawTrackerTiers.cs
+++ src/MonoTorrent/MonoTorrent.Common/RawTrackerTiers.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 using MonoTorrent.BEncoding;
 
-namespace MonoTorrent
+namespace MonoTorrent.Common
 {
     public class RawTrackerTiers : IList<RawTrackerTier>
     {
