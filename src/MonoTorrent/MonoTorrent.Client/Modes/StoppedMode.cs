a6edca54826064fafdbde51ec969cde480cfae6d
aaronsace <aaronsace@hotmail.com>
2018-07-02 18:00:57 +0100

    Fix

--- src/MonoTorrent/MonoTorrent.Client/Modes/StoppedMode.cs
+++ src/MonoTorrent/MonoTorrent.Client/Modes/StoppedMode.cs
@@ -5,33 +5,32 @@ using MonoTorrent.Common;
 
 namespace MonoTorrent.Client
 {
-	class StoppedMode : Mode
-	{
-		public override bool CanHashCheck
-		{
-			get { return true; }
-		}
-		
-		public override TorrentState State
-		{
-			get { return TorrentState.Stopped; }
-		}
+    class StoppedMode : Mode
+    {
+        public override bool CanHashCheck
+        {
+            get { return true; }
+        }
 
-		public StoppedMode(TorrentManager manager)
-			: base(manager)
-		{
-			CanAcceptConnections = false;
-		}
+        public override TorrentState State
+        {
+            get { return TorrentState.Stopped; }
+        }
 
-		public override void HandlePeerConnected(PeerId id, MonoTorrent.Common.Direction direction)
-		{
-			id.CloseConnection();
-		}
+        public StoppedMode(TorrentManager manager)
+            : base(manager)
+        {
+            CanAcceptConnections = false;
+        }
 
+        public override void HandlePeerConnected(PeerId id, MonoTorrent.Common.Direction direction)
+        {
+            id.CloseConnection();
+        }
 
-		public override void Tick(int counter)
-		{
-			// When stopped, do nothing
-		}
-	}
+        public override void Tick(int counter)
+        {
+            // When stopped, do nothing
+        }
+    }
 }
