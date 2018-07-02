c93847e9cb18015f4ad43bba74353ee36c77a48e
aaronsace <aaronsace@hotmail.com>
2018-07-02 18:01:10 +0100

    Fix

--- src/MonoTorrent/MonoTorrent.Client/Modes/StoppingMode.cs
+++ src/MonoTorrent/MonoTorrent.Client/Modes/StoppingMode.cs
@@ -5,52 +5,52 @@ using MonoTorrent.Common;
 
 namespace MonoTorrent.Client
 {
-	class StoppingMode : Mode
-	{
-		WaitHandleGroup handle = new WaitHandleGroup();
+    class StoppingMode : Mode
+    {
+        WaitHandleGroup handle = new WaitHandleGroup();
 
-		public override TorrentState State
-		{
-			get { return TorrentState.Stopping; }
-		}
+        public override TorrentState State
+        {
+            get { return TorrentState.Stopping; }
+        }
 
-		public StoppingMode(TorrentManager manager)
-			: base(manager)
-		{
-			CanAcceptConnections = false;
-			ClientEngine engine = manager.Engine;
-			if (manager.Mode is HashingMode)
-				handle.AddHandle(((HashingMode)manager.Mode).hashingWaitHandle, "Hashing");
+        public StoppingMode(TorrentManager manager)
+            : base(manager)
+        {
+            CanAcceptConnections = false;
+            ClientEngine engine = manager.Engine;
+            if (manager.Mode is HashingMode)
+                handle.AddHandle(((HashingMode)manager.Mode).hashingWaitHandle, "Hashing");
 
-			if (manager.TrackerManager.CurrentTracker != null && manager.TrackerManager.CurrentTracker.Status == TrackerState.Ok)
-				handle.AddHandle(manager.TrackerManager.Announce(TorrentEvent.Stopped), "Announcing");
+            if (manager.TrackerManager.CurrentTracker != null && manager.TrackerManager.CurrentTracker.Status == TrackerState.Ok)
+                handle.AddHandle(manager.TrackerManager.Announce(TorrentEvent.Stopped), "Announcing");
 
-			foreach (PeerId id in manager.Peers.ConnectedPeers)
-				if (id.Connection != null)
-					id.Connection.Dispose();
+            foreach (PeerId id in manager.Peers.ConnectedPeers)
+                if (id.Connection != null)
+                    id.Connection.Dispose();
 
-			manager.Peers.ClearAll();
+            manager.Peers.ClearAll();
 
-			handle.AddHandle(engine.DiskManager.CloseFileStreams(manager), "DiskManager");
+            handle.AddHandle(engine.DiskManager.CloseFileStreams(manager), "DiskManager");
 
-			manager.Monitor.Reset();
-			manager.PieceManager.Reset();
-			engine.ConnectionManager.CancelPendingConnects(manager);
-			engine.Stop();
-		}
+            manager.Monitor.Reset();
+            manager.PieceManager.Reset();
+            engine.ConnectionManager.CancelPendingConnects(manager);
+            engine.Stop();
+        }
 
-		public override void HandlePeerConnected(PeerId id, MonoTorrent.Common.Direction direction)
-		{
-			id.CloseConnection();
-		}
+        public override void HandlePeerConnected(PeerId id, MonoTorrent.Common.Direction direction)
+        {
+            id.CloseConnection();
+        }
 
-		public override void Tick(int counter)
-		{
-			if (handle.WaitOne(0, true))
-			{
-				handle.Close();
-				Manager.Mode = new StoppedMode(Manager);
-			}
-		}
-	}
+        public override void Tick(int counter)
+        {
+            if (handle.WaitOne(0, true))
+            {
+                handle.Close();
+                Manager.Mode = new StoppedMode(Manager);
+            }
+        }
+    }
 }
