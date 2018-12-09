54ee87ee5076bf6156eada2645d8d0eee45272aa
Aaron Simmonds <aaronsace@multipoolminer.io>
2018-12-09 00:07:56 +0000

    Fix magnet stuck on metadata when resuming

--- src/MonoTorrent/MonoTorrent.Client/Managers/TorrentManager.cs
+++ src/MonoTorrent/MonoTorrent.Client/Managers/TorrentManager.cs
@@ -385,6 +385,8 @@ namespace MonoTorrent.Client
 
         public TorrentManager(MagnetLink magnetLink, string savePath, TorrentSettings settings, string torrentSave)
         {
+            Torrent torrent;
+
             Check.MagnetLink(magnetLink);
             Check.InfoHash(magnetLink.InfoHash);
             Check.SavePath(savePath);
@@ -397,6 +399,15 @@ namespace MonoTorrent.Client
             IList<RawTrackerTier> announces = new RawTrackerTiers ();
             if (magnetLink.AnnounceUrls != null)
                 announces.Add (magnetLink.AnnounceUrls);
+
+            if(Torrent.TryLoad(torrentSave, out torrent) && torrent.InfoHash == magnetLink.InfoHash)
+            {
+                this.torrent = torrent;
+                Initialise(savePath, "", announces);
+                ChangePicker(CreateStandardPicker());
+                return;
+            }
+
             Initialise(savePath, "", announces);
         }
 
