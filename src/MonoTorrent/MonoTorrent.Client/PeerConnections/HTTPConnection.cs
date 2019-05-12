3d3742078b52b5dc8b76df1c8a6b122a5ef437e5
Alan McGovern <alan@xamarin.com>
2019-05-12 21:28:30 +0100

    [Client] Don't use a partial class here

--- src/MonoTorrent/MonoTorrent.Client/PeerConnections/HTTPConnection.cs
+++ src/MonoTorrent/MonoTorrent.Client/PeerConnections/HTTPConnection.cs
@@ -46,10 +46,31 @@ using System.Threading.Tasks;
 
 namespace MonoTorrent.Client.Connections
 {
-    public partial class HttpConnection : IConnection
+    public class HttpConnection : IConnection
     {
         static MethodInfo method = typeof(WebHeaderCollection).GetMethod
                                 ("AddWithoutValidate", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        class HttpRequestData
+        {
+            public RequestMessage Request;
+            public bool SentLength;
+            public bool SentHeader;
+            public int TotalToReceive;
+            public int TotalReceived;
+
+            public bool Complete
+            {
+                get { return TotalToReceive == TotalReceived; }
+            }
+
+            public HttpRequestData(RequestMessage request)
+            {
+                Request = request;
+                PieceMessage m = new PieceMessage(request.PieceIndex, request.StartOffset, request.RequestLength);
+                TotalToReceive = m.ByteLength;
+            }
+        }
 
         #region Member Variables
 
