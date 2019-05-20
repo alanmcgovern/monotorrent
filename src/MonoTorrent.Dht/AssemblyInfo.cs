be2ca600cdb8c04fbb3ae5a0338ebbb396e9918d
Alan McGovern <alan.mcgovern@gmail.com>
2019-05-20 14:56:47 +0100

    Move MonoTorrent.Dht inside MonoTorrent again

--- src/MonoTorrent.Dht/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.IO;
-
-[assembly: InternalsVisibleTo("MonoTorrent.Tests")]
-[assembly: AssemblyCopyright("Alan McGovern, Olivier Dufour 2009")]
-[assembly: AssemblyDescription("MonoTorrent DHT")]
