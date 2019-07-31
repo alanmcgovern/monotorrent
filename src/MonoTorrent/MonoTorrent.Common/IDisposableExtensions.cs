0dab4e3d36a2fe989abd72133aff0a49a482c2f2
Alan McGovern <alan@xamarin.com>
2019-07-31 11:22:21 +0100

    [core] Add 'SafeDispose' which wraps Dispose in a try/catch
    We can't guarantee that user subclasses of things like IConnection
    won't throw exceptions, so let's keep ourselves as robust as possible
    against issues.

---
+++ src/MonoTorrent/MonoTorrent.Common/IDisposableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MonoTorrent
+{
+    public static class IDisposableExtensions
+    {
+        public static void SafeDispose (this IDisposable disposable)
+        {
+            try {
+                disposable.Dispose ();
+            } catch {
+                // Ignore
+            }
+        }
+    }
+}
