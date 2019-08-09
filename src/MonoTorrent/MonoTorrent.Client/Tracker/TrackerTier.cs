c90a4956e72706f389f134ee92e1caeb520f021e
Alan McGovern <alan@xamarin.com>
2019-08-09 13:08:00 +0100

    [core] Clean up TrackerTier
    Expose the trackers as a readonly IList.

--- src/MonoTorrent/MonoTorrent.Client/Tracker/TrackerTier.cs
+++ src/MonoTorrent/MonoTorrent.Client/Tracker/TrackerTier.cs
@@ -1,82 +1,62 @@
+//
+// TrackerTier.cs
+//
+// Authors:
+//   Alan McGovern <alan.mcgovern@gmail.com>
+//
+// Copyright (C) 2006 Alan McGovern
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Collections;
-using MonoTorrent.Common;
 
 namespace MonoTorrent.Client.Tracker
 {
-    public class TrackerTier : IEnumerable<ITracker>
+    public class TrackerTier
     {
-        #region Properties
-
-        List<ITracker> OriginalTrackers { get; }
+        public IList<ITracker> Trackers { get; }
 
         internal bool SendingStartedEvent { get; set; }
-
         internal bool SentStartedEvent { get; set; }
-
-        internal List<ITracker> Trackers { get; }
-
-        #endregion Properties
-
-
-        #region Constructors
 
         internal TrackerTier(IEnumerable<string> trackerUrls)
         {
-            Uri result;
-            List<ITracker> trackerList = new List<ITracker>();
-
-            foreach (string trackerUrl in trackerUrls)
-            {
-                // FIXME: Debug spew?
-                if (!Uri.TryCreate(trackerUrl, UriKind.Absolute, out result))
-                {
-                    Logger.Log(null, "TrackerTier - Invalid tracker Url specified: {0}", trackerUrl);
+            var trackerList = new List<ITracker>();
+            foreach (string trackerUrl in trackerUrls) {
+                if (!Uri.TryCreate(trackerUrl, UriKind.Absolute, out Uri result)) {
+                    Logger.Log (null, "TrackerTier - Invalid tracker Url specified: {0}", trackerUrl);
                     continue;
                 }
 
-                ITracker tracker = TrackerFactory.Create(result);
-                if (tracker != null)
-                {
+                var tracker = TrackerFactory.Create(result);
+                if (tracker != null) {
                     trackerList.Add(tracker);
-                }
-                else
-                {
-                    Console.Error.WriteLine("Unsupported protocol {0}", result);                // FIXME: Debug spew?
+                } else {
+                    Logger.Log (null, "Unsupported protocol {0}", result);
                 }
             }
 
-            OriginalTrackers = new List<ITracker>(trackerList);
-            Trackers = trackerList;
+            Trackers = trackerList.AsReadOnly ();
         }
-
-        #endregion Constructors
-
-
-        #region Methods
-
-        internal int IndexOf(ITracker tracker)
-        {
-            return Trackers.IndexOf(tracker);
-        }
-
-        public IEnumerator<ITracker> GetEnumerator()
-        {
-            return Trackers.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public List<ITracker> GetTrackers()
-        {
-            return new List<ITracker>(OriginalTrackers);
-        }
-
-        #endregion Methods
     }
 }
