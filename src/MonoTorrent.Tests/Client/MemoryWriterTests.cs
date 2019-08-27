e6cf286d9fcf0959c2ee629d241c635c1f3c1012
Alan McGovern <alan@xamarin.com>
2019-08-27 17:48:16 +0100

    [core] Clean up MemoryWriter and MemoryWriterTests
    Add some APIs so we can check the cache hits and misses from
    the MemoryWriter.

--- src/MonoTorrent.Tests/Client/MemoryWriterTests.cs
+++ src/MonoTorrent.Tests/Client/MemoryWriterTests.cs
@@ -1,5 +1,5 @@
 //
-// DiskWriterTests.cs
+// MemoryWriterTests.cs
 //
 // Authors:
 //   Alan McGovern alan.mcgovern@gmail.com
@@ -27,40 +27,27 @@
 //
 
 
-using System.Threading.Tasks;
+using System.Linq;
+
 using NUnit.Framework;
 
 namespace MonoTorrent.Client.PieceWriters
 {
 	public class MemoryWriterTests
 	{
-        byte[] buffer;
         MemoryWriter level1;
         MemoryWriter level2;
 
-        TorrentFile singleFile => singleTorrentManager.Torrent.Files [0];
-        TorrentManager singleTorrentManager;
+        TorrentFile file;
 
-        TorrentFile[] multiFile => multiTorrentManager.Torrent.Files;
-        TorrentManager multiTorrentManager;
+        [SetUp]
+        public void Setup()
+        {
+            file = new TorrentFile ("Relative/Path.txt", Piece.BlockSize * 5);
 
-		[SetUp]
-		public void Setup()
-		{
-            var pieceLength = Piece.BlockSize * 2;
-
-            singleTorrentManager = TestRig.CreateSingleFileManager (Piece.BlockSize * 5, pieceLength);
-            multiTorrentManager = TestRig.CreateMultiFileManager (new TorrentFile[] {
-                new TorrentFile ("first", Piece.BlockSize - 550),
-                new TorrentFile ("second", 100),
-                new TorrentFile ("third", Piece.BlockSize)
-            }, pieceLength);
-            buffer = new byte[Piece.BlockSize];
-
-            Initialise(buffer, 1);
-			level2 = new MemoryWriter(new NullWriter(), Piece.BlockSize * 3);
+            level2 = new MemoryWriter(new NullWriter(), Piece.BlockSize * 3);
             level1 = new MemoryWriter(level2, Piece.BlockSize * 3);
-		}
+        }
 
         [Test]
         public void FillFirstBuffer()
@@ -68,77 +55,45 @@ namespace MonoTorrent.Client.PieceWriters
             // Write 4 blocks to the stream and then verify they can all be read
             for (int i = 0; i < 4; i++)
             {
-                Initialise(buffer, (byte)(i + 1));
-                level1.Write(singleFile, Piece.BlockSize * i, buffer, 0, buffer.Length);
+                var buffer = Enumerable.Repeat ((byte)(i + 1), Piece.BlockSize).ToArray ();
+                level1.Write(file, Piece.BlockSize * i, buffer, 0, buffer.Length);
             }
+
+            Assert.AreEqual (Piece.BlockSize * 3, level1.CacheUsed, "#1");
+            Assert.AreEqual (Piece.BlockSize, level2.CacheUsed, "#2");
 
             // Read them all back out and verify them
             for (int i = 0; i < 4; i++)
             {
-                level1.Read(singleFile, Piece.BlockSize * i, buffer, 0, Piece.BlockSize);
-                Verify(buffer, (byte)(i + 1));
+                var buffer = new byte[Piece.BlockSize];
+                level1.Read(file, Piece.BlockSize * i, buffer, 0, Piece.BlockSize);
+                Assert.That (buffer, Is.All.EqualTo ((byte)(i + 1)));
             }
+            Assert.AreEqual (Piece.BlockSize * 3, level1.CacheHits, "#3");
+            Assert.AreEqual (Piece.BlockSize, level1.CacheMisses, "#4");
+            Assert.AreEqual (Piece.BlockSize, level2.CacheHits, "#5");
         }
 
         [Test]
         public void ReadWriteBlock()
         {
-            level1.Write(singleFile, 0, buffer, 0, buffer.Length);
-            level1.Read(singleFile, 0, buffer, 0, buffer.Length);
-            Verify(buffer, 1);
+            var buffer = Enumerable.Repeat ((byte) 55, Piece.BlockSize).ToArray ();
+            level1.Write(file, 0, buffer, 0, buffer.Length);
+
+            buffer = new byte[Piece.BlockSize];
+            level1.Read(file, 0, buffer, 0, buffer.Length);
+            Assert.That (buffer, Is.All.EqualTo (55));
         }
 
         [Test]
         public void ReadWriteBlockChangeOriginal()
         {
-            level1.Write(singleFile, 0, buffer, 0, buffer.Length);
-            Initialise(buffer, 5);
-            level1.Read(singleFile, 0, buffer, 0, buffer.Length);
-            Verify(buffer, 1);
-        }
+            var buffer = Enumerable.Repeat ((byte) 5, Piece.BlockSize).ToArray ();
+            level1.Write(file, 0, buffer, 0, buffer.Length);
 
-        [Test]
-        public async Task ReadWriteSpanningBlock()
-        {
-            // Write one block of data to the memory stream. 
-            int file1 = (int)multiFile[0].Length;
-            int file2 = (int)multiFile[1].Length;
-            int file3 = Piece.BlockSize - file1 - file2;
-
-            Initialise(buffer, 1);
-            level1.Write(multiFile[0], 0, buffer, 0, file1);
-
-            Initialise(buffer, 2);
-            level1.Write(multiFile[1], 0, buffer, 0, file2);
-
-            Initialise(buffer, 3);
-            level1.Write(multiFile[2], 0, buffer, 0, file3);
-
-            // Read the block from the memory stream
-            var manager = new DiskManager (new EngineSettings (), level1);
-            await manager.ReadAsync(multiTorrentManager, 0, buffer, Piece.BlockSize);
-
-            // Ensure that the data is in the buffer exactly as expected.
-            Verify(buffer, 0, file1, 1);
-            Verify(buffer, file1, file2, 2);
-            Verify(buffer, file1 + file2, file3, 3);
-        }
-        
-        void Initialise(byte[] buffer, byte value)
-		{
-			for (int i = 0; i < buffer.Length; i++)
-				buffer[i] = value;
-		}
-
-        void Verify(byte[] buffer, byte expected)
-        {
-            Verify(buffer, 0, buffer.Length, expected);
-        }
-
-        void Verify(byte[] buffer, int startOffset, int count, byte expected)
-        {
-            for (int i = startOffset; i < startOffset + count; i++)
-                Assert.AreEqual(buffer[i], expected, "#" + i);
+            buffer = new byte[Piece.BlockSize];
+            level1.Read(file, 0, buffer, 0, buffer.Length);
+            Assert.That(buffer, Is.All.EqualTo ((byte)5), "#2");
         }
 	}
 }
