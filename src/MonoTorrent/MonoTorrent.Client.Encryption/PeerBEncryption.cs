7d461239a21a25bba8be624813643d833f18a2be
Alan McGovern <alan@xamarin.com>
2019-08-29 01:24:15 +0100

    [core] More severe cleanup of the EncryptedSocket code
    This is still mostly superficial changes.

--- src/MonoTorrent/MonoTorrent.Client.Encryption/PeerBEncryption.cs
+++ src/MonoTorrent/MonoTorrent.Client.Encryption/PeerBEncryption.cs
@@ -62,14 +62,13 @@ namespace MonoTorrent.Client.Encryption
         {
             await base.doneSynchronize();
 
-            var VerifyBytes = new byte[20 + VerificationConstant.Length + 4 + 2]; // ... HASH('req2', SKEY) xor HASH('req3', S), ENCRYPT(VC, crypto_provide, len(PadC), PadC, len(IA))
+            var verifyBytes = new byte[20 + VerificationConstant.Length + 4 + 2]; // ... HASH('req2', SKEY) xor HASH('req3', S), ENCRYPT(VC, crypto_provide, len(PadC), PadC, len(IA))
 
-            await ReceiveMessage(VerifyBytes, VerifyBytes.Length);
-            await gotVerification(VerifyBytes);
+            await ReceiveMessage(verifyBytes, verifyBytes.Length);
+            await gotVerification(verifyBytes);
         }
 
-        byte[] b;
-        private async Task gotVerification(byte[] VerifyBytes)
+        private async Task gotVerification(byte[] verifyBytes)
         {
             byte[] torrentHash = new byte[20];
 
@@ -77,52 +76,40 @@ namespace MonoTorrent.Client.Encryption
             byte[] myCP = new byte[4];
             byte[] lenPadC = new byte[2];
 
-            Array.Copy(VerifyBytes, 0, torrentHash, 0, torrentHash.Length); // HASH('req2', SKEY) xor HASH('req3', S)
+            Array.Copy(verifyBytes, 0, torrentHash, 0, torrentHash.Length); // HASH('req2', SKEY) xor HASH('req3', S)
 
             if (!MatchSKEY(torrentHash))
                 throw new EncryptionException("No valid SKey found");
 
             CreateCryptors("keyB", "keyA");
 
-            DoDecrypt(VerifyBytes, 20, 14); // ENCRYPT(VC, ...
+            DoDecrypt(verifyBytes, 20, 14); // ENCRYPT(VC, ...
 
-            Array.Copy(VerifyBytes, 20, myVC, 0, myVC.Length);
+            Array.Copy(verifyBytes, 20, myVC, 0, myVC.Length);
             if (!Toolbox.ByteMatch(myVC, VerificationConstant))
                 throw new EncryptionException("Verification constant was invalid");
 
-            Array.Copy(VerifyBytes, 28, myCP, 0, myCP.Length); // ...crypto_provide ...
+            Array.Copy(verifyBytes, 28, myCP, 0, myCP.Length); // ...crypto_provide ...
                 
             // We need to select the crypto *after* we send our response, otherwise the wrong
             // encryption will be used on the response
-            b = myCP;
-            Array.Copy(VerifyBytes, 32, lenPadC, 0, lenPadC.Length); // ... len(padC) ...
-            PadC = new byte[DeLen(lenPadC) + 2];
-            await ReceiveMessage(PadC, PadC.Length); // padC            
-            await gotPadC();
-        }
+            Array.Copy(verifyBytes, 32, lenPadC, 0, lenPadC.Length); // ... len(padC) ...
+            var padC = new byte[DeLen(lenPadC) + 2];
+            await ReceiveMessage(padC, padC.Length); // padC
 
-        private async Task gotPadC()
-        {
-            DoDecrypt(PadC, 0, PadC.Length);
+            DoDecrypt(padC, 0, padC.Length);
 
             byte[] lenInitialPayload = new byte[2]; // ... len(IA))
-            Array.Copy(PadC, PadC.Length - 2, lenInitialPayload, 0, 2);
+            Array.Copy(padC, padC.Length - 2, lenInitialPayload, 0, 2);
 
             InitialData = new byte[DeLen(lenInitialPayload)]; // ... ENCRYPT(IA)
             await ReceiveMessage(InitialData, InitialData.Length);
-            await gotInitialPayload();
-        }
-
-        private async Task gotInitialPayload()
-        {
             DoDecrypt(InitialData, 0, InitialData.Length); // ... ENCRYPT(IA)
-            await StepFour();
-        }
 
-        private async Task StepFour()
-        {
+            // Step Four
             byte[] padD = GeneratePad();
-            SelectCrypto(b, false);
+            SelectCrypto(myCP, false);
+
             // 4 B->A: ENCRYPT(VC, crypto_select, len(padD), padD)
             byte[] buffer = new byte[VerificationConstant.Length + CryptoSelect.Length + 2 + padD.Length];
                 
@@ -135,9 +122,8 @@ namespace MonoTorrent.Client.Encryption
             DoEncrypt(buffer, 0, buffer.Length);
             await NetworkIO.SendAsync(socket, buffer, 0, buffer.Length, null, null, null).ConfigureAwait (false);
 
-            SelectCrypto(b, true);
+            SelectCrypto(myCP, true);
         }
-
 
         /// <summary>
         /// Matches a torrent based on whether the HASH('req2', SKEY) xor HASH('req3', S) matches, where SKEY is the InfoHash of the torrent
