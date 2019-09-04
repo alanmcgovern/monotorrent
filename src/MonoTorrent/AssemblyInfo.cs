25d31275b872a097541456df7f7ef99b8b29ca37
Alan McGovern <alan@xamarin.com>
2019-09-04 21:02:38 +0100

    [build] Make it easier to make alpha/beta releases
    Create (and commit!) a file called alpha.txt or beta.txt
    to postfix releases as alpha or beta.

--- src/MonoTorrent/AssemblyInfo.cs
+++ src/MonoTorrent/AssemblyInfo.cs
@@ -4,4 +4,3 @@ using System.Runtime.CompilerServices;
 using System.IO;
 
 [assembly: InternalsVisibleTo("MonoTorrent.Tests")]
-[assembly: AssemblyCopyright("Alan McGovern, Gregor Buguer;  MIT X11 Licensed")]
