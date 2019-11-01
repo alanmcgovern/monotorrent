alanmcgovern/monotorrent

commit 6b01976bc4c1c715e3519bbd601e04cebfa91d93

[core] Add the ability to pause hashing
Adds the feature requested in https://github.com/mono/monotorrent/issues/138

src/MonoTorrent/MonoTorrent.Client/Managers/TorrentManager.cs
@@ -521,15 +521,13 @@ namespace MonoTorrent.Client
         {
             await ClientEngine.MainLoop;
             CheckRegisteredAndDisposed();
-            if (State != TorrentState.Downloading && State != TorrentState.Seeding)
-                return;
 
-            // By setting the state to "paused", peers will not be dequeued from the either the
-            // sending or receiving queues, so no traffic will be allowed.
-            Mode = new PausedMode(this, Engine.DiskManager, Engine.ConnectionManager, Engine.Settings);
-            this.SaveFastResume();
+            if (Mode is HashingMode hashing) {
+                hashing.Pause ();
+            } else if (State == TorrentState.Downloading && State == TorrentState.Seeding) {
+                Mode = new PausedMode(this, Engine.DiskManager, Engine.ConnectionManager, Engine.Settings);
+            }
         }
-
 
         /// <summary>
         /// Starts the TorrentManager
@@ -548,6 +546,8 @@ namespace MonoTorrent.Client
             // make sure the peers begin sending/receiving again
             if (State == TorrentState.Paused) {
                 Mode = new DownloadMode(this, Engine.DiskManager, Engine.ConnectionManager, Engine.Settings);
+            } else if (Mode is HashingMode hashing) {
+                hashing.Resume ();
             } else if (!HasMetadata) {
                 StartTime = DateTime.Now;
                 Mode = new MetadataMode(this, Engine.DiskManager, Engine.ConnectionManager, Engine.Settings, torrentSave);

src/MonoTorrent.Tests/MonoTorrent.Client.Modes/HashingModeTests.cs
@@ -88,6 +88,24 @@ namespace MonoTorrent.Client.Modes
         }
 
         [Test]
+        public async Task PauseResumeHashingMode ()
+        {
+            var pieceHashed = new TaskCompletionSource<object> ();
+            Manager.PieceHashed += (o, e) => pieceHashed.TrySetResult (null);
+
+            var mode = new HashingMode (Manager, DiskManager, ConnectionManager, Settings);
+            Manager.Mode = mode;
+            mode.Pause ();
+
+            var hashingTask = mode.WaitForHashingToComplete ();
+            await Task.Delay (50);
+            Assert.IsFalse (pieceHashed.Task.IsCompleted, "#1");
+
+            mode.Resume ();
+            Assert.AreEqual (pieceHashed.Task, await Task.WhenAny (pieceHashed.Task, Task.Delay (1000)), "#2");
+        }
+
+        [Test]
         public async Task ReadZeroFromDisk ()
         {
             PieceWriter.FilesThatExist.AddRange(new[]{

src/MonoTorrent/MonoTorrent.Client/Modes/HashingMode.cs
@@ -38,13 +38,33 @@ namespace MonoTorrent.Client.Modes
         CancellationTokenSource Cancellation { get; }
 
         public override bool CanHashCheck => false;
-        public override TorrentState State => TorrentState.Hashing;
+
+        bool Paused { get; set; }
+        TaskCompletionSource<object> PausedCompletionSource { get; set; }
+
+        public override TorrentState State => Paused ? TorrentState.HashingPaused : TorrentState.Hashing;
 
         public HashingMode (TorrentManager manager, DiskManager diskManager, ConnectionManager connectionManager, EngineSettings settings)
             : base (manager, diskManager, connectionManager, settings)
         {
             CanAcceptConnections = false;
             Cancellation = new CancellationTokenSource();
+
+            // Mark it as completed so we are *not* paused by default;
+            PausedCompletionSource = new TaskCompletionSource<object> ();
+            PausedCompletionSource.TrySetResult (null);
+        }
+
+        public void Pause ()
+        {
+            PausedCompletionSource?.TrySetResult (null);
+            PausedCompletionSource = new TaskCompletionSource<object> ();
+            Cancellation.Token.Register (() => PausedCompletionSource.TrySetCanceled ());
+        }
+
+        public void Resume ()
+        {
+            PausedCompletionSource.TrySetResult (null);
         }
 
         public Task WaitForHashingToComplete ()
@@ -59,6 +79,10 @@ namespace MonoTorrent.Client.Modes
             try {
                 if (await Manager.Engine.DiskManager.CheckAnyFilesExistAsync (Manager.Torrent)) {
                     for (int index = 0; index < Manager.Torrent.Pieces.Count; index++) {
+
+                        if (!PausedCompletionSource.Task.IsCompleted)
+                            await PausedCompletionSource.Task;
+
                         var hash = await Manager.Engine.DiskManager.GetHashAsync(Manager.Torrent, index);
 
                         if (token.IsCancellationRequested) {
@@ -70,6 +94,9 @@ namespace MonoTorrent.Client.Modes
                         Manager.OnPieceHashed (index, hashPassed);
                     }
                 } else {
+                    if (!PausedCompletionSource.Task.IsCompleted)
+                        await PausedCompletionSource.Task;
+
                     for (int i = 0; i < Manager.Torrent.Pieces.Count; i++)
                         Manager.OnPieceHashed(i, false);
                 }

src/MonoTorrent/MonoTorrent/Enums.cs
@@ -47,6 +47,7 @@ namespace MonoTorrent.Client
         Downloading,
         Seeding,
         Hashing,
+        HashingPaused,
         Stopping,
         Error,
 		Metadata

commit 634ac17138bfd18803615191e8d0619f637e7a68

[core] Fix a regression in HashCheckAsync from 6b01976bc4c1
When the ability to pause/resume hashing was added it broke the
logic used by HashCheckAsync (true) and the TorrentManager no
longer moved to Downloading/Seeding mode.

Fixes https://github.com/mono/monotorrent/issues/155

src/MonoTorrent/MonoTorrent.Client/Managers/TorrentManager.cs
@@ -589,7 +589,7 @@ namespace MonoTorrent.Client
             // make sure the peers begin sending/receiving again
             if (State == TorrentState.Paused) {
                 Mode = new DownloadMode(this, Engine.DiskManager, Engine.ConnectionManager, Engine.Settings);
-            } else if (Mode is HashingMode hashing) {
+            } else if (Mode is HashingMode hashing && !HashChecked) {
                 hashing.Resume ();
             } else if (!HasMetadata) {
                 StartTime = DateTime.Now;

src/MonoTorrent.Tests/MonoTorrent.Client.Modes/HashingModeTests.cs
@@ -107,6 +107,20 @@ namespace MonoTorrent.Client.Modes
         }
 
         [Test]
+        public async Task HashCheckAsync_Autostart ()
+        {
+            await Manager.HashCheckAsync (true);
+            Assert.AreEqual (TorrentState.Downloading, Manager.State, "#1");
+        }
+
+        [Test]
+        public async Task HashCheckAsync_DoNotAutostart ()
+        {
+            await Manager.HashCheckAsync (false);
+            Assert.AreEqual (TorrentState.Stopped, Manager.State, "#1");
+        }
+
+        [Test]
         public async Task PauseResumeHashingMode ()
         {
             var pieceHashed = new TaskCompletionSource<object> ();