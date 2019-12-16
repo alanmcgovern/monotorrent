67977f5b46cc1c443062032ac7b9edb94b1df0a9
Alan McGovern <alan@xamarin.com>
2019-12-16 09:43:00 +0000

    [core] Don't special case peers which are handshaking
    Once a connection has been established to a remote peer, or
    an incoming connection has been received, we should add the
    peer to the 'Connected' list.
    
    This way any logic which attempts to close all open connections
    will successfully close connections which are still completing
    the normal bittorrent handshake.

--- src/MonoTorrent.Tests/MonoTorrent.Client.Modes/DownloadModeTests.cs
+++ src/MonoTorrent.Tests/MonoTorrent.Client.Modes/DownloadModeTests.cs
@@ -177,7 +177,10 @@ namespace MonoTorrent.Client.Modes
             Assert.IsTrue (Peer.Connection.Connected, "#1");
             Manager.HandlePeerConnected (Peer);
             Assert.IsTrue (Peer.Connection.Connected, "#2");
-            Assert.IsTrue (Manager.Peers.ConnectedPeers.Contains (Peer), "#3");
+
+            // ConnectionManager should add the PeerId to the Connected list whenever
+            // an outgoing connection is made, or an incoming one is received.
+            Assert.IsFalse (Manager.Peers.ConnectedPeers.Contains (Peer), "#3");
         }
 
         [Test]
