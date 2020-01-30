ee1fc1b0352539d4f873a2d3220bb9fb7f329d0b
Alan McGovern <alan.mcgovern@gmail.com>
2020-01-30 07:14:02 +0000

    [core] Add allocation-free BinarySearch overloads
    These overloads allow passing a state object, which
    means we can pass a ValueTuple and have allocation-free
    binary searching when we need to use local method state
    to find the location we care about.
    
    Thanks to @therzok for pushing me to do it ;)

---
+++ src/MonoTorrent/MonoTorrent/IListExtensions.cs
@@ -0,0 +1,71 @@
+﻿//
+// IListExtensions.cs
+//
+// Authors:
+//   Alan McGovern alan.mcgovern@gmail.com
+//
+// Copyright (C) 2020 Alan McGovern
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+
+using System;
+using System.Collections.Generic;
+
+namespace MonoTorrent
+{
+    static class IListExtensions
+    {
+        public static int BinarySearch<T> (this IList<T> list, Func<T, int> predicate)
+        {
+            int min = 0;
+            int max = list.Count - 1;
+            while (min <= max) {
+                var mid = (min + max) / 2;
+                var result = predicate (list[mid]);
+                if (result == 0)
+                    return mid;
+                if (result < 0)
+                    min = mid + 1;
+                if (result > 0)
+                    max = mid - 1;
+            }
+            return ~min;
+        }
+
+        public static int BinarySearch<T, TState> (this IList<T> list, Func<T, TState, int> predicate, TState comparand)
+        {
+            int min = 0;
+            int max = list.Count - 1;
+            while (min <= max) {
+                var mid = (min + max) / 2;
+                var result = predicate (list[mid], comparand);
+                if (result == 0)
+                    return mid;
+                if (result < 0)
+                    min = mid + 1;
+                if (result > 0)
+                    max = mid - 1;
+            }
+            return ~min;
+        }
+    }
+}
