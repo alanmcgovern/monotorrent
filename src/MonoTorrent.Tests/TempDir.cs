7afa2f318d70f092e8f3685f035ff5c6302fc7d7
Alan McGovern <alan.mcgovern@gmail.com>
2021-03-12 15:50:09 +0000

    add missing file

---
+++ src/MonoTorrent.Tests/TempDir.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoTorrent
+{
+    static class TempDir
+    {
+        public readonly struct Releaser : IDisposable
+        {
+            public string Path { get; }
+
+            public Releaser (string path)
+                => Path = path;
+
+            public void Dispose ()
+            {
+                Directory.Delete (Path, true);
+            }
+        }
+
+        public static Releaser Create ()
+        {
+            var tmp = Path.GetTempFileName ();
+            var tmpDir = tmp + "_dir";
+            Directory.CreateDirectory (tmpDir);
+            File.Delete (tmp);
+            return new Releaser (tmpDir);
+        }
+    }
+}
