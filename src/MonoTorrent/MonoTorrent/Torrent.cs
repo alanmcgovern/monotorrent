61bcc569b648e6cced86e33101a084cf9ba9d077
Phil Scott <phil.scott@gmail.com>
2021-03-31 19:01:53 -0400

    [core] Verifies valid and absolute HttpSeed urls on load
    Invalid urls would fail to parse in Mode.DownloadLogic throwing an unhandled exception. This adds a check for a well formed and absolute uri before adding them to the HttpSeeds

--- src/MonoTorrent/MonoTorrent/Torrent.cs
+++ src/MonoTorrent/MonoTorrent/Torrent.cs
@@ -13,10 +13,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -676,10 +676,14 @@ namespace MonoTorrent
 
                     case ("url-list"):
                         if (keypair.Value is BEncodedString httpSeedString) {
-                            HttpSeeds.Add (httpSeedString.Text);
+                            if (Uri.IsWellFormedUriString (httpSeedString.Text, UriKind.Absolute)) {
+                                HttpSeeds.Add (httpSeedString.Text);
+                            }
                         } else if (keypair.Value is BEncodedList httpSeedList) {
                             foreach (BEncodedString str in httpSeedList)
-                                HttpSeeds.Add (str.Text);
+                                if (Uri.IsWellFormedUriString (str.Text, UriKind.Absolute)) {
+                                    HttpSeeds.Add (str.Text);
+                                }
                         }
                         break;
 
