05dd67c549898f9f4ad19b47a8cb29876238fb7e
Alan McGovern <alan.mcgovern@gmail.com>
2021-04-25 11:58:39 +0100

    [core] Add the ability to delete data when removing
    If a magnet link is used to create a TorrentManager then
    the metadata is written to the cached directory after it
    is successfully downloaded.
    
    If a filepath is used, then the file will be copied to
    the cache directory.
    
    If a raw 'Torrent' is used, the metadata stored by the
    torrent is written to the cache directory too.
    
    The path to the metadata is exposed by the TorrentManager.
    
    'ClientEngine.RemoveAsync' now has an overload to control
    which data should be deleted when the torrent is removed.
    You can remove nothing, the cache data, the downloaded data,
    or all of the above.

---
+++ src/MonoTorrent/MonoTorrent.Client/RemoveMode.cs
@@ -0,0 +1,61 @@
+﻿//
+// RemoveMode.cs
+//
+// Authors:
+//   Alan McGovern alan.mcgovern@gmail.com
+//
+// Copyright (C) 2021 Alan McGovern
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+
+namespace MonoTorrent.Client
+{
+    public enum RemoveMode
+    {
+        /// <summary>
+        /// Does not remove any cache data, such as fast resume data and the copy of the .torrent metadata,
+        /// from the <see cref="EngineSettings.CacheDirectory"/> when removing the <see cref="TorrentManager"/>
+        /// from the <see cref="ClientEngine"/>. Any data downloaded by the <see cref="TorrentManager"/> will not be deleted.
+        /// </summary>
+        KeepAllData = 0,
+
+        /// <summary>
+        /// Removes all cache data, such as fast resume data and the copy of the .torrent metadata,
+        /// from the <see cref="EngineSettings.CacheDirectory"/> when removing the <see cref="TorrentManager"/>
+        /// from the <see cref="ClientEngine"/>. Any data downloaded by the <see cref="TorrentManager"/> will not be deleted.
+        /// </summary>
+        CacheDataOnly = 1 << 0,
+
+        /// <summary>
+        /// Any data downloaded by the <see cref="TorrentManager"/> will be deleted. Does not remove any cache data,
+        /// such as fast resume data and the copy of the .torrent metadata, from the <see cref="EngineSettings.CacheDirectory"/>
+        /// when removing the <see cref="TorrentManager"/> from the <see cref="ClientEngine"/>.
+        /// </summary>
+        DownloadedDataOnly = 1 << 1,
+
+        /// <summary>
+        /// Removes all cache data from the <see cref="EngineSettings.CacheDirectory"/> when removing the <see cref="TorrentManager"/>
+        /// from the <see cref="ClientEngine"/>. Any data downloaded by the <see cref="TorrentManager"/> will be deleted.
+        /// </summary>
+        CacheDataAndDownloadedData = CacheDataOnly | DownloadedDataOnly,
+    }
+}
