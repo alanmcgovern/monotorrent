454a6a1fd4a5917abb4316398a79458b0843a755
alan.mcgovern <alan.mcgovern@gmail.com>
2021-12-02 12:34:54 +0000

    [samples] Add a basic BenchmarkDotNet project
    Useful for microbenchmarks

---
+++ src/Benchmark/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using MonoTorrent;
+using MonoTorrent.BEncoding;
+using MonoTorrent.Messages.Peer;
+
+namespace MyBenchmarks
+{
+    [MemoryDiagnoser]
+    public class MessageEncoder
+    {
+        readonly Memory<byte> Buffer;
+        readonly RequestMessage Message;
+
+        public MessageEncoder ()
+        {
+            Message = new RequestMessage (1, 2, 3);
+            Buffer = new Memory<byte> (new byte[20]);
+            Message.Encode (Buffer.Span);
+        }
+
+        [Benchmark]
+        public void Encode ()
+        {
+            Message.Encode (Buffer.Span);
+        }
+
+        [Benchmark]
+        public void Decode ()
+        {
+            Message.Decode (Buffer.Span);
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class BEncodingBenchmark
+    {
+        readonly byte[] Buffer;
+        readonly Stream Stream;
+        public BEncodingBenchmark ()
+        {
+            Buffer = File.ReadAllBytes ("c:\\ubuntu-20.10-live-server-amd64.iso.torrent");
+            Stream = new MemoryStream (Buffer);
+        }
+
+        [Benchmark]
+        public void OldDecode ()
+        {
+            Stream.Position = 0;
+            BEncodedDictionary.DecodeTorrent (Stream);
+        }
+
+        [Benchmark]
+        public void NewDecode () => BEncodedDictionary.DecodeTorrent (Buffer);
+    }
+
+    [MemoryDiagnoser]
+    public class SequenceEqual
+    {
+        readonly BEncodedString Value1 = "c:\\ubuntu-20.10-live-server-amd64.iso.torrent";
+        readonly BEncodedString Value2 = "c:\\ubuntu-20.10-live-server-amd64.iso.torrent";
+
+        [Benchmark]
+        public void OldCompare () => Value1.Equals (Value2);
+
+        [Benchmark]
+        public void NewCompare () => MemoryExtensions.SequenceEqual (Value1.Span, Value2.Span);
+
+    }
+
+    [MemoryDiagnoser]
+    public class BEncodedNumberBenchmark
+    {
+        readonly byte[] buffer = new byte[30];
+        readonly BEncodedNumber MinValue = long.MinValue;
+        readonly BEncodedNumber MaxValue = long.MaxValue;
+        readonly BEncodedNumber ZeroValue = 0;
+        readonly BEncodedNumber OneValue = 1;
+
+        public BEncodedNumberBenchmark ()
+        {
+            MaxValue.Encode (buffer);
+        }
+
+        [Benchmark]
+        public void Smallest () => MinValue.Encode (buffer);
+
+        [Benchmark]
+        public void Largest () => MaxValue.Encode (buffer);
+
+        [Benchmark]
+        public void Zero () => ZeroValue.Encode (buffer);
+
+        [Benchmark]
+        public void One () => OneValue.Encode (buffer);
+    }
+
+    [MemoryDiagnoser]
+    public class BEncodedNumberLengthInBytesBenchmark
+    {
+        readonly BEncodedNumber MinValue = long.MinValue;
+        readonly BEncodedNumber MaxValue = long.MaxValue;
+        readonly BEncodedNumber ZeroValue = 0;
+        readonly BEncodedNumber OneValue = 1;
+
+        public BEncodedNumberLengthInBytesBenchmark ()
+        {
+        }
+
+        [Benchmark]
+        public void Smallest () => MinValue.LengthInBytes ();
+
+        [Benchmark]
+        public void Largest () => MaxValue.LengthInBytes ();
+
+        [Benchmark]
+        public void Zero () => ZeroValue.LengthInBytes ();
+
+        [Benchmark]
+        public void One () => OneValue.LengthInBytes ();
+    }
+
+    public class Program
+    {
+        public static void Main (string[] args)
+        {
+            var summary = BenchmarkRunner.Run (typeof (BEncodedNumberLengthInBytesBenchmark));
+        }
+    }
+}
