f54c4ead433ac7ef7541636bd7f884e9930201ad
Alan McGovern <alan.mcgovern@gmail.com>
2022-01-24 00:16:29 +0000

    [core] Use CloseAsync instead of FlushAsync
    When changing from Downloading mode to Seeding mode we
    should close any open PieceWriters so that the underlying
    FileStreams (if any) will be closed. This way they can
    be re-opened in FileAccess.Read mode, rather than
    being left in FileAccess.ReadWrite mode.

--- src/MonoTorrent.Client/MonoTorrent.Client.Modes/DownloadMode.cs
+++ src/MonoTorrent.Client/MonoTorrent.Client.Modes/DownloadMode.cs
@@ -98,7 +98,7 @@ namespace MonoTorrent.Client.Modes
                 await Task.WhenAll (
                     Manager.TrackerManager.AnnounceAsync (TorrentEvent.Completed, CancellationToken.None).AsTask (),
                     Manager.MaybeWriteFastResumeAsync ().AsTask (),
-                    DiskManager.FlushAsync (Manager)
+                    DiskManager.CloseFilesAsync (Manager)
                 );
                 Manager.RaiseTorrentStateChanged (new TorrentStateChangedEventArgs (Manager, TorrentState.Downloading, TorrentState.Seeding));
             } else if (Manager.PartialProgressSelector.TrueCount > 0) {
@@ -108,7 +108,7 @@ namespace MonoTorrent.Client.Modes
                     state = TorrentState.Seeding;
                     await Task.WhenAll (
                         Manager.MaybeWriteFastResumeAsync ().AsTask (),
-                        DiskManager.FlushAsync (Manager)
+                        DiskManager.CloseFilesAsync (Manager)
                     );
                     Manager.RaiseTorrentStateChanged (new TorrentStateChangedEventArgs (Manager, TorrentState.Downloading, TorrentState.Seeding));
                 } else if (Manager.Bitfield.CountTrue (Manager.PartialProgressSelector) < Manager.PartialProgressSelector.TrueCount && state == TorrentState.Seeding) {
