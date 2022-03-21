0dfb913f8f68e3a6c961350b3875a82e91ae94af
issueg2k4g34j2g <101990239+issueg2k4g34j2g@users.noreply.github.com>
2022-03-21 23:26:56 +0200

    Disregard certain path segments
    This *should* solve #517, but I don't know C# 🙃

--- src/MonoTorrent.Client/MonoTorrent/Torrent.cs
+++ src/MonoTorrent.Client/MonoTorrent/Torrent.cs
@@ -636,8 +636,10 @@ namespace MonoTorrent
 
                         case ("path.utf-8"):
                             foreach (BEncodedString str in ((BEncodedList) keypair.Value)) {
-                                sb.Append (str.Text);
-                                sb.Append (Path.DirectorySeparatorChar);
+                                if (!(string.IsNullOrEmpty (str.Text) || str.Text == Path.DirectorySeparatorChar)) {
+                                    sb.Append (str.Text);
+                                    sb.Append (Path.DirectorySeparatorChar);
+                                }
                             }
                             path = sb.ToString (0, sb.Length - 1);
                             sb.Remove (0, sb.Length);
@@ -646,8 +648,10 @@ namespace MonoTorrent
                         case ("path"):
                             if (string.IsNullOrEmpty (path)) {
                                 foreach (BEncodedString str in ((BEncodedList) keypair.Value)) {
-                                    sb.Append (str.Text);
-                                    sb.Append (Path.DirectorySeparatorChar);
+                                    if (!(string.IsNullOrEmpty (str.Text) || str.Text == Path.DirectorySeparatorChar)) {
+                                        sb.Append (str.Text);
+                                        sb.Append (Path.DirectorySeparatorChar);
+                                    }
                                 }
                                 path = sb.ToString (0, sb.Length - 1);
                                 sb.Remove (0, sb.Length);
