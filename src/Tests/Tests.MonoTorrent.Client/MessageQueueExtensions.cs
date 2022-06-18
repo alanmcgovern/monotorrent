5526963b38dec4977204529f7666090704af9fb2
alan.mcgovern <alan.mcgovern@gmail.com>
2022-06-18 13:23:43 +0100

    [core] Add a helper method for tests

--- src/Tests/Tests.MonoTorrent.Client/MessageQueueExtensions.cs
+++ src/Tests/Tests.MonoTorrent.Client/MessageQueueExtensions.cs
@@ -5,11 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 
 using MonoTorrent.Messages.Peer;
+using MonoTorrent.Messages.Peer.Libtorrent;
 
 namespace MonoTorrent.Client
 {
     static class MessageQueueExtensions
     {
+        public static PeerId AddConnectedPeer (this TorrentManager manager, bool supportsLTMetdata = false)
+        {
+            var peer = PeerId.CreateNull (manager.Bitfield.Length);
+            manager.Peers.ConnectedPeers.Add (peer);
+            if (supportsLTMetdata) {
+                peer.SupportsFastPeer = true;
+                peer.SupportsLTMessages = true;
+                peer.ExtensionSupports.Add (LTMetadata.Support);
+            }
+            return peer;
+        }
+
         public static PeerMessage TryDequeue (this MessageQueue queue)
             => queue.TryDequeue (out PeerMessage message, out PeerMessage.Releaser releaser) ? message : null;
     }
