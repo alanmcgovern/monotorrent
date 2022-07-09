36ae567e32112d9f51cf75e827da4490a72d3a87
Alan McGovern <alan.mcgovern@gmail.com>
2022-07-10 00:47:14 +0100

    [dht] Fix an initialization issue
    Handle adding nodes to the engine before calling StartAsync.

---
+++ src/Tests/Tests.MonoTorrent.Dht/MonoTorrent.Dht/DhtEngineTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace MonoTorrent.Dht
+{
+    [TestFixture]
+    public class DhtEngineTests
+    {
+        [Test]
+        public void AddRawNodesBeforeStarting ()
+        {
+            int count = 0;
+            var engine = new DhtEngine ();
+            engine.MessageLoop.QuerySent += (o, e) => count++;
+            engine.Add (new ReadOnlyMemory<byte>[] { new byte[100] });
+            Assert.AreEqual (0, engine.MessageLoop.PendingQueries);
+            Assert.AreEqual (0, count);
+            Assert.AreEqual (0, engine.RoutingTable.CountNodes ());
+            Assert.AreEqual (0, engine.MessageLoop.DhtMessageFactory.RegisteredMessages);
+        }
+    }
+}
