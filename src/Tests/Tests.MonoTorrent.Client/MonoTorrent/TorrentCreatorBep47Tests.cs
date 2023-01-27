alanmcgovern/monotorrent

commit 3d3937dbdfe44a81923db9d04c25c9c6a1ee0296

[core] Sort files before creating hybrid or v2 torrents
As the underlying storage is a bencoded dictionary, whose
keys must be sorted, you *must* sort the list of files which
will be placed in a hybrid or v2 torrent prior to hashing
them, otherwise the order of the files will be flipped and
piece hashes won't match anymore.

This doesn't appear to be mentioned in the spec, but is
called out in a code comment in the sample torrent creator
linked as part of the bep52 spec.

            # the directory entries must be processed in lexicographic order so that
            # the files list matches the bencoded ordering of the file tree

I went hunting as soon as i realised i didn't know how to
make this test pass as the files were not in a bencoded list.

Fixes https://github.com/alanmcgovern/monotorrent/issues/563

Test case written by @borigas

src/MonoTorrent.Client/MonoTorrent/TorrentCreator.cs
@@ -259,6 +259,13 @@ namespace MonoTorrent
                 var info = (file.Destination, length, padding, file.Source);
                 return info;
             }).ToArray ();
+
+            // Hybrid and V2 torrents *must* hash files in the same order as they end up being stored in the bencoded dictionary,
+            // which means they must be alphabetical.
+            if (Type.HasV2 ())
+                rawFiles = rawFiles.OrderBy (t => t.Destination).ToArray ();
+
+            // The last file never has padding bytes
             rawFiles[rawFiles.Length - 1].padding = 0;
 
             var files = TorrentFileInfo.Create (PieceLength, rawFiles);

src/Tests/Tests.MonoTorrent.Client/MonoTorrent/TorrentCreatorTests.cs
@@ -261,6 +261,32 @@ namespace MonoTorrent.Common
         }
 
         [Test]
+        public async Task CreateV2Torrent_SortFilesCorrectly ()
+        {
+            using var releaser = TempDir.Create ();
+
+            var dir = new DirectoryInfo (releaser.Path);
+            var eFile = new FileInfo (Path.Combine (dir.FullName, "E.file"));
+            File.WriteAllText (eFile.FullName, "aoeu");
+
+            var bFile = new FileInfo (Path.Combine (dir.FullName, "B.file"));
+            File.WriteAllText (bFile.FullName, "aoeu");
+
+            var aFileInDir = new FileInfo (Path.Combine (dir.FullName, "Dir/A.file"));
+            if (!aFileInDir.Directory.Exists)
+                aFileInDir.Directory.Create ();
+            File.WriteAllText (aFileInDir.FullName, "aoeu");
+
+            ITorrentFileSource fileSource = new TorrentFileSource (dir.FullName);
+            TorrentCreator torrentCreator = new TorrentCreator (TorrentType.V1V2Hybrid);
+            var encodedTorrent = await torrentCreator.CreateAsync (fileSource);
+            var torrent = Torrent.Load (encodedTorrent);
+
+            Assert.IsNotNull (torrent);
+        }
+
+
+        [Test]
         public void CannotCreateTorrentWithAllEmptyFiles ([Values (TorrentType.V1Only, TorrentType.V1V2Hybrid, TorrentType.V2Only)] TorrentType torrentType)
         {
             // Create a torrent from files with all zeros

commit 9746cfb5e9b87d2ffd4e39457300dabe6a39da4c

[core] Fix a BEP52 related regression
When sorting files for BEP52 compatibility, use an ordinal
comparison rather than the default culture aware comparison.

BitTorrent relies on ordinal comparisons when sorting keys
inside BEncodedDictionaries, and that is the order which
must be respected here.

Fixes a regression from https://github.com/alanmcgovern/monotorrent/commit/3d3937dbdfe44a81923db9d04c25c9c6a1ee0296

Fixes https://github.com/alanmcgovern/monotorrent/issues/610

src/MonoTorrent.Client/MonoTorrent/TorrentCreator.cs
@@ -235,7 +235,7 @@ namespace MonoTorrent
             if (mappings.Count == 0)
                 throw new ArgumentException ("The file source must contain one or more files", nameof (fileSource));
 
-            mappings.Sort ((left, right) => left.Destination.CompareTo (right.Destination));
+            mappings.Sort ((left, right) => StringComparer.Ordinal.Compare (left.Destination, right.Destination));
             Validate (mappings);
 
             return await CreateAsync (fileSource.TorrentName, fileSource, token);
@@ -263,7 +263,7 @@ namespace MonoTorrent
             // Hybrid and V2 torrents *must* hash files in the same order as they end up being stored in the bencoded dictionary,
             // which means they must be alphabetical.
             if (Type.HasV2 ())
-                rawFiles = rawFiles.OrderBy (t => t.Destination).ToArray ();
+                rawFiles = rawFiles.OrderBy (t => t.Destination, StringComparer.Ordinal).ToArray ();
 
             // The last file never has padding bytes
             rawFiles[rawFiles.Length - 1].padding = 0;

src/Tests/Tests.MonoTorrent.Client/MonoTorrent/TorrentCreatorBep47Tests.cs
@@ -74,6 +74,25 @@ namespace MonoTorrent.Common
         }
 
         [Test]
+        [TestCase(TorrentType.V1Only)]
+        [TestCase(TorrentType.V1OnlyWithPaddingFiles)]
+        [TestCase(TorrentType.V1V2Hybrid)]
+        [TestCase(TorrentType.V2Only)]
+        public async Task CreateHybridWithUnusualFilenames (TorrentType type)
+        {
+            var files = new Source {
+                TorrentName = "asd",
+                Files = new[] {
+                    new FileMapping("ASDkfsdjgsdSDFGsj.asd", "ASDkfsdjgsdSDFGsj.asd", (long)(PieceLength * 2.30)),
+                    new FileMapping("aSvzxkaSqp AZXDCaj asdASDjas ASDl.aaaaaa", "aSvzxkaSqp AZXDCaj asdASDjas ASDl.aaaaaa", (long)(PieceLength * 36.5)),
+                    new FileMapping("[aaaaa aaaaaaaaa]aaaaaa a aaaaaaaa.aaaaa", "[aaaaa aaaaaaaaa]aaaaaa a aaaaaaaa.aaaaa", (long)(PieceLength * 3.17)),
+                }
+            };
+            var torrent = await CreateTestBenc (type, files);
+            Assert.DoesNotThrow (() => Torrent.Load (torrent));
+        }
+
+        [Test]
         public async Task FileLengthSameAsPieceLength ([Values (TorrentType.V1Only, TorrentType.V1V2Hybrid)] TorrentType type)
         {
             var files = new Source {