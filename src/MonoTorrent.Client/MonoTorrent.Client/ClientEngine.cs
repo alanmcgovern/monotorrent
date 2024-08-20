1d2df66e3f31f811d6dc2ddf7291a98a38c59b30
Alan McGovern <alanmcgovern@users.noreply.github.com>
2024-08-20 22:25:41 +0100

    Expose IPeerConnectionListeners publicly (#679)
    * Expose IPeerConnectionListeners publicly
    
    This makes it easier for everyone to determine which specific local
    ports are bound when '0' is used as the preferred port.

--- src/MonoTorrent.Client/MonoTorrent.Client/ClientEngine.cs
+++ src/MonoTorrent.Client/MonoTorrent.Client/ClientEngine.cs
@@ -231,7 +231,12 @@ namespace MonoTorrent.Client
 
         internal Factories Factories { get; }
 
-        internal IList<IPeerConnectionListener> PeerListeners { get; set; } = Array.Empty<IPeerConnectionListener> ();
+
+        /// <summary>
+        /// A readonly list of the listeners which the engine is using to receive incoming connections from other peers.
+        /// This are created by passing <see cref="EngineSettings.ListenEndPoints"/> to the <see cref="Factories.CreatePeerConnectionListener(IPEndPoint)"/> factory method.
+        /// </summary>
+        public IList<IPeerConnectionListener> PeerListeners { get; private set; } = Array.Empty<IPeerConnectionListener> ();
 
         internal ILocalPeerDiscovery LocalPeerDiscovery { get; private set; }
 
