7286722240b3870321b68fea47a53e58f9bdb164
alan.mcgovern <alan.mcgovern@gmail.com>
2026-06-28 10:54:24 +0100

    Implement UtpPacket as per library norms
    The underlying data will be a Memory<byte> so the timestamps can be
    written right before sending so having a 'ReadOnlyMemory' version for
    sending is not particularly useful.
    
    The intended flow will be:
    - Engine calls 'SendAsync' with a buffer of data.
    - The UTP connection will reformat this data into N packets and enqueue
      them for sending.
    - The 'SendAsync' will be marked as 'done' so the engine knows more data
      than be sent.
    - If the send queue is full, then the utp connection will not allow
      subsequent 'SendAsync' calls to return until queue space has freed up
      and the latest message has been reformatted and enqueued.

---
+++ src/MonoTorrent.Connections/MonoTorrent.Connections.Peer/UtpPeerConnection.cs
@@ -0,0 +1,70 @@
+﻿//
+// UtpPeerConnection.cs
+//
+// Authors:
+//   Alan McGovern alan.mcgovern@gmail.com
+//
+// Copyright (C) 2026 Alan McGovern
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using ReusableTasks;
+
+namespace MonoTorrent.Connections.Peer
+{
+    class UtpPeerConnection : IPeerConnection
+    {
+        public ReadOnlyMemory<byte> AddressBytes { get; }
+        public bool CanReconnect { get; }
+        public bool Disposed { get; }
+        public IPEndPoint? EndPoint { get; }
+        public bool IsIncoming { get; }
+        public Uri Uri { get; }
+
+        public ReusableTask<bool> ConnectAsync ()
+        {
+            throw new NotImplementedException ();
+        }
+
+        public void Dispose ()
+        {
+            throw new NotImplementedException ();
+        }
+
+        public ReusableTask<int> ReceiveAsync (Memory<byte> buffer)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public ReusableTask<int> SendAsync (ReadOnlyMemory<byte> buffer)
+        {
+            throw new NotImplementedException ();
+        }
+    }
+}
