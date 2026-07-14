3e25dfe61a4a49e4861dbe1c599e96ac713c715b
Alan McGovern <alanmcgovern@users.noreply.github.com>
2026-07-14 21:52:26 +0100

    Apply suggestion from @alanmcgovern

--- src/Samples/SampleClient/main.cs
+++ src/Samples/SampleClient/main.cs
@@ -51,7 +51,6 @@ namespace ClientSample
                 // it's cache directory. Otherwise the metadata will be downloaded and stored in the cache directory
                 // so it can be reloaded later.
                 AutoSaveLoadMagnetLinkMetadata = true,
-                AllowedTransports =ImmutableArray.Create (PeerTransport.Utp),
                 // Use a fixed port to accept incoming connections from other peers for testing purposes. Production usages should use a random port, 0, if possible.
                 ListenEndPoints = new Dictionary<string, IPEndPoint> {
                     { "ipv4", new IPEndPoint (IPAddress.Any, 55123) },
